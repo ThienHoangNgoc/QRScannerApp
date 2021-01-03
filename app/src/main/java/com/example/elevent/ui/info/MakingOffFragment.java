24fb39662e570249e8d2f6f8145b467201439261
Thien <legendflashx3@gmail.com>
2021-01-03 23:58:04 +0100

    added MakingOffFragment

--- app/src/main/java/com/example/elevent/ui/info/MakingOffFragment.java
+++ app/src/main/java/com/example/elevent/ui/info/MakingOffFragment.java
@@ -1,9 +1,12 @@
 package com.example.elevent.ui.info;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -11,13 +14,26 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.elevent.R;
+import com.example.elevent.utils.Utils;
 
 public class MakingOffFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_making_off, container, false);
-        TextView text = root.findViewById(R.id.dummy_text_making_off);
+
+        Button playBtn = root.findViewById(R.id.play_making_off_btn);
+
+        playBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(Utils.getStringFromResource(R.string.making_off_link,root.getContext())));
+                startActivity(intent);
+
+            }
+        });
+
         return root;
     }
 }
