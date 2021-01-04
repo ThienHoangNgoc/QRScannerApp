710b349f18edb4fc944181d741719fdd26f80b1e
Thien <legendflashx3@gmail.com>
2021-01-04 14:30:40 +0100

    clean up code

--- app/src/main/java/com/example/elevent/ui/info/adapter/InfoAdapter.java
+++ app/src/main/java/com/example/elevent/ui/info/adapter/InfoAdapter.java
@@ -10,8 +10,6 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.elevent.R;
